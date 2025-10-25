0014001d2c1a17933184e5388facf1b3efb53190
Jon Hermansen <jon@jh86.org>
2025-10-25 07:28:26 +0000

    test

--- modules/system/boot/loader/stand-freebsd/stand-conf-builder.nix
+++ modules/system/boot/loader/stand-freebsd/stand-conf-builder.nix
@@ -1,12 +1,14 @@
 { pkgs, stand-efi, initmd }:
-
+let
+  loaderScript = pkgs.writeText "nixbsd-loader.lua" (builtins.readFile ./nixbsd-loader.lua);
+in
 pkgs.replaceVarsWith {
   src = ./stand-conf-builder.sh;
   isExecutable = true;
   replacements = {
     path = [ pkgs.coreutils pkgs.gnused pkgs.gnugrep pkgs.jq ];
     stand = stand-efi;
-    loader_script = ./nixbsd-loader.lua;
+    loader_script = loaderScript;
     inherit (pkgs) bash;
     initmd = if initmd == null then "" else initmd;
   };
