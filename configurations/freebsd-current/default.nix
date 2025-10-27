bdbda47eb0dfed4965c43e6f8f359ce31a46d411
Jon Hermansen <jon@jh86.org>
2025-10-27 10:18:40 -0400

    it boots and imports the pool. missing nvidia drivers tho

--- configurations/freebsd-current/default.nix
+++ configurations/freebsd-current/default.nix
@@ -1,13 +1,15 @@
 { config, lib, ... }: {
-  imports = [ ../base/default.nix ];
+  imports = [ ../graphical/default.nix ];
 
   nixpkgs.overlays = [
     (import ../../overlays/freebsd-main.nix)
     (final': prev': {
       freebsd = prev'.freebsd.overrideScope (final: prev: {
-        sysctl = prev.sysctl.overrideAttrs {
-          MK_JAIL = "no";
-        };
+        sysctl = prev.sysctl.overrideAttrs (oldAttrs: {
+          buildInputs = (oldAttrs.buildInputs or []) ++ [
+            prev.libjail
+          ];
+        });
         syslogd = prev.syslogd.overrideAttrs (oldAttrs: {
           buildInputs = (oldAttrs.buildInputs or []) ++ [
             prev.libcasper
@@ -24,7 +26,54 @@
             sed -i 's/date -ur /date -ud @/' ../common/newvers.sh
           '';
         });
+        drm-kmod-firmware = prev.drm-kmod-firmware.overrideAttrs (oldAttrs: {
+          hardeningDisable = (oldAttrs.hardeningDisable or []) ++ [ "fortify" ];
+
+          XARGS = "${final.buildFreebsd.xargs-j}/bin/xargs-j";
+          XARGS_J = "";
+        });
       });
+      epoll-shim = prev'.epoll-shim.overrideAttrs (oldAttrs: {
+        hardeningDisable = (oldAttrs.hardeningDisable or []) ++ [ "fortify" ];
+      });
+      mesa = prev'.mesa.override {
+galliumDrivers = [
+    "d3d12" # WSL emulated GPU (aka Dozen)
+    #"iris" # new Intel (Broadwell+)
+    "llvmpipe" # software renderer
+    "nouveau" # Nvidia
+    "r300" # very old AMD
+    "r600" # less old AMD
+    "radeonsi" # new AMD (GCN+)
+    "softpipe" # older software renderer
+    "svga" # VMWare virtualized GPU
+    "virgl" # QEMU virtualized GPU (aka VirGL)
+    "zink" # generic OpenGL over Vulkan, experimental
+];
+vulkanDrivers = [
+    "amd" # AMD (aka RADV)
+    #"intel" # new Intel (aka ANV)
+    "microsoft-experimental" # WSL virtualized GPU (aka DZN/Dozen)
+    "swrast" # software renderer (aka Lavapipe)
+#] ++ lib.optionals stdenv.hostPlatform.isx86 [
+#  #"intel_hasvk" # Intel Haswell/Broadwell, "legacy" Vulkan driver (https://www.phoronix.com/news/Intel-HasVK-Drop-Dead-Code)
+];
+#eglPlatforms = [ "x11" "wayland" ]
+#vulkanLayers = [
+#  "device-select"
+#  "overlay"
+#  "intel-nullhw"
+#];
+      };
+      sway = prev'.sway.override {
+        #enableXwayland = false;
+      };
+      swaybg = prev'.swaybg.overrideAttrs {
+        hardeningDisable = [ "stackprotector" "fortify" ];
+      };
+      swaylock = prev'.swaylock.overrideAttrs {
+        hardeningDisable = [ "stackprotector" "fortify" ];
+      };
     })
   ];
 
--- configurations/graphical/default.nix
+++ configurations/graphical/default.nix
@@ -8,16 +8,27 @@
     ((pkg.meta or {}).sourceProvenance or []) == [ lib.sourceTypes.binaryFirmware ];
 
   programs.sway.enable = true;
+  #programs.sway.extraPackages = [];
 
   services.dbus.enable = true;
-  services.xserver = {
-    enable = true;
-    displayManager.lightdm.enable = true;
-    displayManager.defaultSession = "xfce";
-    desktopManager.xfce = {
-      enable = true;
-    };
-    exportConfiguration = true;
-    libinput.enable = true; # for touchpad support on many laptops
-  };
+#  services.xserver = {
+#    enable = true;
+#    displayManager.lightdm.enable = true;
+#    displayManager.defaultSession = "xfce";
+#    desktopManager.xfce = {
+#      enable = true;
+#    };
+#    exportConfiguration = true;
+#    libinput.enable = true; # for touchpad support on many laptops
+#  };
+
+  environment.xfce.excludePackages = with pkgs; [
+    xfce.parole
+    xfce.ristretto
+    xfce.xfce4-screenshooter
+    xfce.xfce4-notifyd
+  ];
+  #services.pulseaudio.enable = false;
+  #services.pipewire.pulse.enable = false;
+  #security.polkit.enable = lib.mkForce false;
 }
